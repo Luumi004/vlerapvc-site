7949d42f9b3be67f3e305c442373ea10a9496a5f
Luumi004 <lumi.aliji@gmail.com>
2026-03-23 19:37:40 +0100

    Updated contact information

--- components/contact-section.tsx
+++ components/contact-section.tsx
@@ -28,8 +28,8 @@ export default function ContactSection() {
                 <h4 className="font-medium text-gray-800">
                   {t("contact.phone")}
                 </h4>
-                <p className="text-gray-600">+389 70 271 289</p>
                 <p className="text-gray-600">+41 76 213 19 16</p>
+                <p className="text-gray-600">+389 70 378 389</p>
               </div>
             </div>
 
